9db1fe2323070e1958636c9bcc13254da1c09b87
Murad khan <murad.jaddoon@gmail.com>
2022-08-24 23:08:37 +0500

    imported header at app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import ThemeConfig from "../theme";
+import { Header } from "../layout";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeConfig>
+      <Header />
       <Component {...pageProps} />
     </ThemeConfig>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
